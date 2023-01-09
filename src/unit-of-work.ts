7ac7bb906bf73b69458981aa42aef6ef00d54079
ing.dacastillop <ing.dacastillop@gmail.com>
2023-01-09 13:37:23 -0500

    :rocket: Change implementation of UnitOfWork

--- src/unit-of-work.ts
+++ src/unit-of-work.ts
@@ -1,19 +1,29 @@
 import { UnitOfWork } from '@xofttion/clean-architecture';
 import { TypeormEntityDatabase } from './entity-database';
+import { TypeormEntityDataSource } from './entity-datasource';
 import { TypeormEntityManager } from './entity-manager';
 import { CoopplinsTypeormSql } from './sql-manager';
 
 export class TypeormUnitOfWork implements UnitOfWork {
-  constructor(
-    private _database: TypeormEntityDatabase,
-    private _manager: TypeormEntityManager
-  ) {}
+  private _database: TypeormEntityDatabase;
+  private _manager: TypeormEntityManager;
+
+  constructor() {
+    const dataSource = new TypeormEntityDataSource();
+
+    this._database = new TypeormEntityDatabase();
+    this._manager = new TypeormEntityManager(dataSource);
+  }
+
+  public get manager(): TypeormEntityManager {
+    return this._manager;
+  }
 
   public async flush(): Promise<void> {
-    const runner = CoopplinsTypeormSql.createRunner();
+    try {
+      const runner = CoopplinsTypeormSql.createRunner();
 
-    if (runner) {
-      try {
+      if (runner) {
         this._database.setRunner(runner);
         this._manager.setRunner(runner);
 
@@ -24,13 +34,13 @@ export class TypeormUnitOfWork implements UnitOfWork {
         await this._manager.flush();
 
         await this._database.commit();
-      } catch (ex) {
-        await this._database.rollback();
-
-        throw ex;
-      } finally {
-        await this._database.disconnect();
       }
+    } catch (ex) {
+      await this._database.rollback();
+
+      throw ex;
+    } finally {
+      await this._database.disconnect();
     }
   }
 }
